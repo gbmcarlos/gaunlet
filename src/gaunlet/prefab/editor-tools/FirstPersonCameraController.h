fe6a6f7457b4d11886551a75a707eddf18f01152
Carlos <gbmcarlos@gmail.com>
2022-09-18 13:22:07 +0400

    complete the first-person camera controller

--- src/gaunlet/prefab/editor-tools/FirstPersonCameraController.h
+++ src/gaunlet/prefab/editor-tools/FirstPersonCameraController.h
@@ -4,6 +4,8 @@
 #include "gaunlet/core/window/Input.h"
 #include "gaunlet/core/window/KeyCodes.h"
 #include "gaunlet/editor/workspace/Workspace.h"
+#include "gaunlet/core/event/events/MouseEvent.h"
+#include "gaunlet/core/event/events/CursorEvent.h"
 
 namespace gaunlet::Prefab::EditorTools {
 
@@ -12,7 +14,7 @@ namespace gaunlet::Prefab::EditorTools {
     public:
 
         FirstPersonCameraController(const char* renderPanelId)
-            : FirstPersonCameraController(renderPanelId, 2.0f, 15.0f) {}
+            : FirstPersonCameraController(renderPanelId, 2.0f, 1.0f) {}
 
         FirstPersonCameraController(const char* renderPanelId, float movementSensitivity, float rotationSensitivity)
             : m_renderPanelId(renderPanelId), m_movementSensitivity(movementSensitivity), m_rotationSensitivity(rotationSensitivity) {}
@@ -23,36 +25,50 @@ namespace gaunlet::Prefab::EditorTools {
 
         void onUpdate(Core::TimeStep timeStep) override {
 
-            auto renderPanel = getWorkspace()->getRenderPanel(m_renderPanelId);
-            auto& camera = getWorkspace()->getCamera(renderPanel->getCameraId());
+            selectRenderPanel();
+            if (m_renderPanel == nullptr) {
+                return;
+            }
 
-            float movement = 1.0f * timeStep * m_movementSensitivity;
-            float rotation = 1.0f * timeStep * m_rotationSensitivity;
+            auto& camera = getWorkspace()->getCamera(m_renderPanel->getCameraId());
 
-            // ROTATION
+            float movement = timeStep * m_movementSensitivity;
+            float rotation = timeStep * m_rotationSensitivity;
+
+            bool shiftPressed = Core::Input::isKeyPressed(GL_KEY_LEFT_SHIFT) || Core::Input::isKeyPressed(GL_KEY_RIGHT_SHIFT);
+
+            // MOVEMENT
             if (Core::Input::isKeyPressed(GL_KEY_UP)) {
-                camera->addRotation(0, rotation);
+                if (shiftPressed) {
+                    camera->moveSemiRelative({0, movement, 0});
+                } else {
+                    camera->moveRelative({0, movement, 0});
+                }
             }
 
             if (Core::Input::isKeyPressed(GL_KEY_DOWN)) {
-                camera->addRotation(0, -rotation);
-            }
-
-            if (Core::Input::isKeyPressed(GL_KEY_RIGHT)) {
-                camera->addRotation(rotation, 0);
-            }
-
-            if (Core::Input::isKeyPressed(GL_KEY_LEFT)) {
-                camera->addRotation(-rotation, 0);
+                if (shiftPressed) {
+                    camera->moveSemiRelative({0, -movement, 0});
+                } else {
+                    camera->moveRelative({0, -movement, 0});
+                }
             }
 
             // MOVEMENT
             if (Core::Input::isKeyPressed(GL_KEY_W)) {
-                camera->moveRelative({0, 0, movement});
+                if (shiftPressed) {
+                    camera->moveSemiRelative({0, 0, -movement});
+                } else {
+                    camera->moveRelative({0, 0, -movement});
+                }
             }
 
             if (Core::Input::isKeyPressed(GL_KEY_S)) {
-                camera->moveRelative({0, 0, -movement});
+                if (shiftPressed) {
+                    camera->moveSemiRelative({0, 0, movement});
+                } else {
+                    camera->moveRelative({0, 0, movement});
+                }
             }
 
             if (Core::Input::isKeyPressed(GL_KEY_D)) {
@@ -63,13 +79,93 @@ namespace gaunlet::Prefab::EditorTools {
                 camera->moveRelative({-movement, 0, 0});
             }
 
+            // ROTATION
+            if (m_rotating) {
+                camera->addRotation(-m_rotationDelta.x * rotation, m_rotationDelta.y * rotation);
+            }
+
+        }
+
+        bool onEvent(gaunlet::Core::Event &event) override {
+
+            gaunlet::Core::EventDispatcher dispatcher(event);
+            dispatcher.dispatch<gaunlet::Core::MouseButtonPress>(GL_BIND_CALLBACK_FN(FirstPersonCameraController::onMousePressEvent));
+            dispatcher.dispatch<gaunlet::Core::MouseButtonRelease>(GL_BIND_CALLBACK_FN(FirstPersonCameraController::onMouseReleaseEvent));
+            dispatcher.dispatch<gaunlet::Core::CursorMoveEvent>(GL_BIND_CALLBACK_FN(FirstPersonCameraController::onCursorMoveEvent));
+
+            return true;
+
+        }
+
+        bool onMousePressEvent(gaunlet::Core::MouseButtonPress& event) {
+
+            selectRenderPanel();
+            if (m_renderPanel == nullptr) {
+                return true;
+            }
+
+            m_rotating = true;
+
+            m_initialPosition = {
+                m_renderPanel->getMousePositionX(),
+                m_renderPanel->getMousePositionY()
+            };
+            m_rotationDelta = {0, 0};
+
+            return true;
+        }
+
+        bool onMouseReleaseEvent(gaunlet::Core::MouseButtonRelease& event) {
+
+            m_rotating = false;
+            return true;
+
+        }
+
+        bool onCursorMoveEvent(gaunlet::Core::CursorMoveEvent& event) {
+
+            if (!m_rotating) {
+                return true;
+            }
+
+            glm::vec2 currentPosition = {
+                m_renderPanel->getMousePositionX(),
+                m_renderPanel->getMousePositionY()
+            };
+
+            m_rotationDelta = m_initialPosition - currentPosition;
+
+            return true;
+
+        }
+
+        void selectRenderPanel() {
+
+            if (m_renderPanelId) {
+
+                // If a render panel id was specified, make sure it's focused
+                Editor::RenderPanel* renderPanel = getWorkspace()->getRenderPanel(m_renderPanelId);
+                if (renderPanel->isFocused()) {
+                    m_renderPanel = renderPanel;
+                } else {
+                    m_renderPanel = nullptr;
+                }
+
+            } else {
+                m_renderPanel = getWorkspace()->getFocusedRenderPanel();
+            }
+
         }
 
     private:
 
         const char* m_renderPanelId;
+        Editor::RenderPanel* m_renderPanel;
         float m_movementSensitivity;
         float m_rotationSensitivity;
+        bool m_rotating = false;
+        glm::vec2 m_initialPosition = {};
+        glm::vec2 m_rotationDelta = {};
 
     };
 
